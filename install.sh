3be9f5694dfbe3f9bd7d627af40a25d59017fba9
Roberto Rolo <robertorolo@gmail.com>
2016-06-02 14:45:57 -0300

    add install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+AR2GEMS_PATH=$HOME
+PYTHON_PY_PATH="${AR2GEMS_PATH}/ar2gems/bin/plugins/Geostat/python/"
+PYTHON_UI_PATH="${AR2GEMS_PATH}/ar2gems/bin/plugins/Geostat/"
+echo "UI path=${PYTHON_UI_PATH}"
+echo "PYTHON path=${PYTHON_PY_PATH}"
+
+cp *.ui $PYTHON_UI_PATH
+cp *.py $PYTHON_PY_PATH
+
+
